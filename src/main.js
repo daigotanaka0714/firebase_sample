f37a8d735d194de1535f8ccd3203bc232b548b2f
Daigo Tanaka <daigo.tanaka0714@gmail.com>
2019-07-23 18:25:42 +0900

    firebase情報追記

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ import './registerServiceWorker'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDuFBdU_Ty_icyz-NACraCKSWmqI82Cdr4",
+  authDomain: "fir-sample-187c1.firebaseapp.com",
+  databaseURL: "https://fir-sample-187c1.firebaseio.com",
+  projectId: "fir-sample-187c1",
+  storageBucket: "",
+  messagingSenderId: "15146301276",
+  appId: "1:15146301276:web:9ac5757230ec8c1b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
